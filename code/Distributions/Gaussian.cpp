750fc91c569e82befef801292c6126f6fcdb7608
João Faria <joao.faria@unige.ch>
2025-01-14 17:52:19 +0100

    new TruncatedGaussian distribution

--- code/Distributions/Gaussian.cpp
+++ code/Distributions/Gaussian.cpp
@@ -61,4 +61,47 @@ double HalfGaussian::log_pdf(double x) const
 }
 
 
+TruncatedGaussian::TruncatedGaussian(double center, double width, double lower, double upper)
+: center(center), width(width), lower(lower), upper(upper)
+{
+    if(width <= 0.0)
+        throw std::domain_error("TruncatedGaussian distribution must have positive width.");
+    if(lower >= upper)
+        throw std::domain_error("TruncatedGaussian: lower bound should be less than upper bound.");
+    alpha = (lower - center) / width;
+    beta = (upper - center) / width;
+
+    Z = normal_cdf(beta) - normal_cdf(alpha);
+    // some combinations of center, width, lower, upper are numerically unstable
+    // TODO: calculations in log to solve this?
+    if (Z == 0.0)
+        throw std::domain_error("TruncatedGaussian: numerically imprecise, check input parameters.");
+}
+
+double TruncatedGaussian::cdf(double x) const
+{
+    if (x < lower)
+        return 0.0;
+    if (x > upper)
+        return 1.0;
+    double r = (x - center) / width;
+    return (normal_cdf(r) - normal_cdf(alpha)) / Z;
+}
+
+double TruncatedGaussian::cdf_inverse(double x) const
+{
+    if(x < 0.0 || x > 1.0)
+        throw std::domain_error("Input to cdf_inverse must be in [0, 1].");
+    double x_cdf = Z * x + normal_cdf(alpha);
+    return center + width * normal_inverse_cdf(x_cdf);
+}
+
+double TruncatedGaussian::log_pdf(double x) const
+{
+    if( (x < lower) || (x > upper) )
+        return -std::numeric_limits<double>::infinity();
+	double r = (x - center)/width;
+    return -0.5*r*r - log(width) - _norm_pdf_logC - log(Z);
+}
+
 } // namespace DNest4
--- code/Distributions/Gaussian.h
+++ code/Distributions/Gaussian.h
@@ -1,6 +1,7 @@
 #ifndef DNest4_Gaussian
 #define DNest4_Gaussian
 
+#include <limits>
 #include "ContinuousDistribution.h"
 #include "../RNG.h"
 #include "../Utils.h"
@@ -49,6 +50,32 @@ class HalfGaussian:public ContinuousDistribution
         virtual std::ostream& print(std::ostream& out) const override
         {
             out << "HalfGaussian(" << width << ")";
+            return out;
+        }
+};
+
+class TruncatedGaussian:public ContinuousDistribution
+{
+    private:
+        const double _norm_pdf_logC = log(sqrt(2 * M_PI));
+
+    public:
+        // Location and scale parameter
+        double center, width;
+        double lower, upper;
+        double alpha, beta, Z;
+
+        TruncatedGaussian(double center=0.0, double width=1.0,
+                          double lower=-std::numeric_limits<double>::infinity(),
+                          double upper=std::numeric_limits<double>::infinity());
+
+        double cdf(double x) const override;
+        double cdf_inverse(double p) const override;
+        double log_pdf(double x) const override;
+
+        virtual std::ostream& print(std::ostream& out) const override
+        {
+            out << "TruncatedGaussian(" << center << "; " << width << "; [" << lower << ", " << upper << "])";
             return out;
         }
 };
